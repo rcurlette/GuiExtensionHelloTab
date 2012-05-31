b7a0ffd78dabd8da6c5893ecbbaa0b7245e7925e
Robert Curlette <robert@curlette.com>
2012-05-31 15:42:11 +0200

    Adding files

---
+++ HelloTab.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="HelloTab.ascx.cs" Inherits="HelloTab.HelloTab" %>
+<asp:Literal ID="output" runat="server"></asp:Literal>
+<div id="compUri"></div>
---
+++ HelloTab.js
@@ -0,0 +1,33 @@
+﻿Type.registerNamespace("RC");
+
+RC.HelloTab = function RC$HelloTab$HelloTab(element) {
+    console.log('Constructor');
+    Tridion.OO.enableInterface(this, "RC.HelloTab");
+    this.addInterface("Tridion.Controls.DeckPage", [element]); //My extension is like this
+};
+
+RC.HelloTab.prototype.initialize = function HelloTab$initialize()
+{
+    console.log('init');
+    this.callBase("Tridion.Controls.DeckPage", "initialize");
+    $evt.addEventHandler($display.getItem(), "load", this.getDelegate(this.updateView));
+};
+
+RC.HelloTab.prototype.select = function HelloTab$select() {
+    var c = $display.getItem();
+    $j("#compUri").text(c.getId());
+    console.log('select');
+    this.callBase("Tridion.Controls.DeckPage", "select");
+    this.updateView();
+};
+
+RC.HelloTab.prototype.updateView = function HelloTab$updateView()
+{
+    console.log('update');
+    if (this.isSelected()) 
+    {
+        console.log('selected')           
+    }
+};
+
+Tridion.Controls.Deck.registerPageType(RC.HelloTab, "RC.HelloTab");
